100f65af41d6a760e936436735f14f62aee1c613
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-04-23 14:04:06 +0200

    add LennaHeadBruit

--- NLinuxEdEnviTI/run.sh
+++ NLinuxEdEnviTI/run.sh
@@ -1,19 +1,19 @@
 cd ./bin
 
 # Create the image Simple Scan
-./EdSimpleScan.exe ../Image/LenaHead.pgm ../ImRes/LennaHeadBruitSimpleScan.pgm
+./EdSimpleScan.exe ../Image/LennaHeadBruit.pgm ../ImRes/LennaHeadBruitSimpleScan.pgm
 
 # Create the image Scan Vois 3
-./EdScanVois3.exe ../Image/LenaHead.pgm ../ImRes/LennaHeadBruitScanVois3.pgm
+./EdScanVois3.exe ../Image/LennaHeadBruit.pgm ../ImRes/LennaHeadBruitScanVois3.pgm
 
 # Create the image Threshold
-./EdThreshold.exe ../Image/LenaHead.pgm ../ImRes/LennaHeadBruitThreshold.pgm
+./EdThreshold.exe ../Image/LennaHeadBruit.pgm ../ImRes/LennaHeadBruitThreshold.pgm
 
 # Create the image Mean Filtering
-./EdMeanFiltering.exe ../Image/LenaHead.pgm ../ImRes/LennaHeadBruitMeanFiltering.pgm
+./EdMeanFiltering.exe ../Image/LennaHeadBruit.pgm ../ImRes/LennaHeadBruitMeanFiltering.pgm
 
 # Create the image Gauss Filtering
-./EdGaussFiltering.exe ../Image/LenaHead.pgm ../ImRes/LennaHeadBruitGaussFiltering.pgm
+./EdGaussFiltering.exe ../Image/LennaHeadBruit.pgm ../ImRes/LennaHeadBruitGaussFiltering.pgm
 
 # Create the image Median Filtering
-./EdMedianFiltering.exe ../Image/LenaHead.pgm ../ImRes/LennaHeadBruitMedianFiltering.pgm
+./EdMedianFiltering.exe ../Image/LennaHeadBruit.pgm ../ImRes/LennaHeadBruitMedianFiltering.pgm
